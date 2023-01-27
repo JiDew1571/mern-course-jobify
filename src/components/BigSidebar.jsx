859d285a82e5269a9f63935c54c9968d72b16d81
JiDew1571 <ji.dew1571@gmail.com>
2023-01-27 16:15:49 +0530

    completed big and small Sidebars

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,6 +1,24 @@
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
 
 const BigSidebar = () => {
-	return <Wrapper>BigSidebar</Wrapper>;
+	const { showSidebar } = useAppContext();
+	return (
+		<Wrapper>
+			<div
+				className={
+					showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+				}>
+				<div className='content'>
+					<header>
+						<Logo />
+					</header>
+					<NavLinks />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 export default BigSidebar;
--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,22 +1,26 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { useAppContext } from '../context/appContext';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
-// import Logo from './Logo';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
+	const { showSidebar, toggleSidebar } = useAppContext();
+
 	return (
 		<Wrapper>
-			<div className='sidebar-container show-sidebar'>
-				<div className='container'>
-					<button className='close-btn'>
+			<div
+				className={
+					showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+				}>
+				<div className='content'>
+					<button type='button' className='close-btn' onClick={toggleSidebar}>
 						<AiFillCloseCircle />
 					</button>
 					<header>
 						<Logo />
 					</header>
-					<div className='nav-links'>nav links</div>
+					<NavLinks toggleSidebar={toggleSidebar} />
 				</div>
 			</div>
 		</Wrapper>
