514c3f2fc1534ff6ae005ebb8581aaa6b82b67a7
JiDew1571 <ji.dew1571@gmail.com>
2023-01-27 16:14:52 +0530

    restricted auths

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -7,9 +7,16 @@ import {
 	updateJob,
 	showStats,
 } from '../controllers/jobController.js';
+import authenticateUser from '../middleware/auth.js';
 
-router.route('/').post(createJob).get(getAllJobs);
-router.route('/stats').get(showStats);
-router.route('/:id').delete(deleteJob).patch(updateJob);
+router
+	.route('/')
+	.post(authenticateUser, createJob)
+	.get(authenticateUser, getAllJobs);
+router.route('/stats').get(authenticateUser, showStats);
+router
+	.route('/:id')
+	.delete(authenticateUser, deleteJob)
+	.patch(authenticateUser, updateJob);
 
 export default router;
